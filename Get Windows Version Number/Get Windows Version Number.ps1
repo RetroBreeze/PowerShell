024743cef3622f3a13815561a281c1c0cc3fa397
sgyll <sgyll@mdlogistics.com>
2016-12-07 11:13:00 -0500

    Added various scripts

---
+++ Get Windows Version Number/Get Windows Version Number.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module ImportExcel
+$path = 'C:/Temp/BuildNumbers.xlsx'
+
+$computers = Get-ADComputer -Filter 'Name -like "mdl*"' -Properties * | Select-Object name,OperatingSystemVersion | Where-Object {($_.name -match '[MDL]\d{3}$' -and $_.name -notmatch 'MDL154' -and $_.name -notmatch 'MDL245')}
+$computers | Export-Excel $path -Show -AutoSize -AutoFilter
