db43834c18eff76887bda4f410a654ad231eab17
sgyll <sgyll@mdlogistics.com>
2016-12-14 15:03:48 -0500

    - Added wildcards for determining device location (also standardizes location name in the output) - Added Reno - Added Default parameter - Added Type and Notes fields (imported from CSV)

--- CheckActiveDevices/CheckActiveDevices.ps1
+++ CheckActiveDevices/CheckActiveDevices.ps1
@@ -7,9 +7,10 @@ $finalOutput = @{}
 
 $deviceList = foreach ($device in $deviceImport)
 {
-
 $deviceIP = $device.IP
 $deviceName = $device.Name
+$deviceType = $device.Type
+$deviceNotes = $device.Notes
 $deviceLocation = $device.Location
 $status = ''
 
@@ -32,40 +33,82 @@ $status = ''
         $status = 'ONLINE'
     }
 
-      <#$obj = New-Object -TypeName PSobject
-        $obj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
-        $obj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
-        $obj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value $deviceLocation
-        $obj | Add-Member -MemberType NoteProperty -Name Status -Value $status#>
-
-        switch($deviceLocation){
-
-        '1301 Perry Road'{
-        $1301obj = New-Object -TypeName PSobject
-        $1301obj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
-        $1301obj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
-        $1301obj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value $deviceLocation
-        $1301obj | Add-Member -MemberType NoteProperty -Name Status -Value $status
-        $1301obj
+    switch -Wildcard ($deviceLocation)
+    {
+        '*1301*'
+        {
+            $1301obj = New-Object -TypeName PSobject
+            $1301obj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
+            $1301obj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
+            $1301obj | Add-Member -MemberType NoteProperty -Name DeviceType -Value $deviceType
+            $1301obj | Add-Member -MemberType NoteProperty -Name DeviceNotes -Value $deviceNotes
+            $1301obj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value '1301 Perry Road'
+            $1301obj | Add-Member -MemberType NoteProperty -Name Status -Value $status
+            $1301obj | Export-Excel $outputFile -WorkSheetname $1301obj.DeviceLocation -AutoSize -BoldTopRow -FreezeTopRow -AutoFilter -NoClobber -ConditionalText $(
+                New-ConditionalText OFFLINE darkred -BackgroundColor pink
+                New-ConditionalText ONLINE darkgreen -BackgroundColor lightgreen
+            )
         }
-        '700 Perry Road'{
-        $700obj = New-Object -TypeName PSobject
-        $700obj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
-        $700obj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
-        $700obj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value $deviceLocation
-        $700obj | Add-Member -MemberType NoteProperty -Name Status -Value $status
-        $700obj
-        }
-        '2150 Stanley Road'{
-        $2150obj = New-Object -TypeName PSobject
-        $2150obj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
-        $2150obj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
-        $2150obj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value $deviceLocation
-        $2150obj | Add-Member -MemberType NoteProperty -Name Status -Value $status
-        $2150obj
-        }
-        } #end switch($devicelocation)
 
+        '*700*'
+        {
+            $700obj = New-Object -TypeName PSobject
+            $700obj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
+            $700obj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
+            $700obj | Add-Member -MemberType NoteProperty -Name DeviceType -Value $deviceType
+            $700obj | Add-Member -MemberType NoteProperty -Name DeviceNotes -Value $deviceNotes
+            $700obj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value '700 Perry Road'
+            $700obj | Add-Member -MemberType NoteProperty -Name Status -Value $status
+            $700obj | Export-Excel $outputFile -WorkSheetname $700obj.DeviceLocation -AutoSize -BoldTopRow -FreezeTopRow -AutoFilter -NoClobber -ConditionalText $(
+                New-ConditionalText OFFLINE darkred -BackgroundColor pink
+                New-ConditionalText ONLINE darkgreen -BackgroundColor lightgreen
+            )
+        }
+
+        '*2150*'
+        {
+            $2150obj = New-Object -TypeName PSobject
+            $2150obj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
+            $2150obj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
+            $2150obj | Add-Member -MemberType NoteProperty -Name DeviceType -Value $deviceType
+            $2150obj | Add-Member -MemberType NoteProperty -Name DeviceNotes -Value $deviceNotes
+            $2150obj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value '2150 Stanley Road'
+            $2150obj | Add-Member -MemberType NoteProperty -Name Status -Value $status
+            $2150obj | Export-Excel $outputFile -WorkSheetname $2150Obj.DeviceLocation -AutoSize -BoldTopRow -FreezeTopRow -AutoFilter -NoClobber -ConditionalText $(
+                New-ConditionalText OFFLINE darkred -BackgroundColor pink
+                New-ConditionalText ONLINE darkgreen -BackgroundColor lightgreen
+            )
+        }
+
+        '*Reno*'
+        {
+            $renoobj = New-Object -TypeName PSobject
+            $renoobj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
+            $renoobj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
+            $renoobj | Add-Member -MemberType NoteProperty -Name DeviceType -Value $deviceType
+            $renoobj | Add-Member -MemberType NoteProperty -Name DeviceNotes -Value $deviceNotes
+            $renoobj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value 'Reno'
+            $renoobj | Add-Member -MemberType NoteProperty -Name Status -Value $status
+            $renoobj | Export-Excel $outputFile -WorkSheetname Reno -AutoSize -BoldTopRow -FreezeTopRow -AutoFilter -NoClobber -ConditionalText $(
+                New-ConditionalText OFFLINE darkred -BackgroundColor pink
+                New-ConditionalText ONLINE darkgreen -BackgroundColor lightgreen
+            )
+        }
+
+        default
+        {
+            $defobj = New-Object -TypeName PSobject
+            $defobj | Add-Member -MemberType NoteProperty -Name DeviceIP -Value $deviceIP
+            $defobj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $deviceName
+            $defobj | Add-Member -MemberType NoteProperty -Name DeviceType -Value $deviceType
+            $defobj | Add-Member -MemberType NoteProperty -Name DeviceNotes -Value $deviceNotes
+            $defobj | Add-Member -MemberType NoteProperty -Name DeviceLocation -Value $deviceLocation
+            $defobj | Add-Member -MemberType NoteProperty -Name Status -Value $status
+            $defobj | Export-Excel $outputFile -WorkSheetname Undetermined -AutoSize -BoldTopRow -FreezeTopRow -AutoFilter -NoClobber -ConditionalText $(
+                New-ConditionalText OFFLINE darkred -BackgroundColor pink
+                New-ConditionalText ONLINE darkgreen -BackgroundColor lightgreen
+            )
+        }
+
+    } #end switch($devicelocation)
 }#end foreach ($device in $deviceImport)
-
-ExportExcel -Show -AutoSize $outputFile $devicelist
